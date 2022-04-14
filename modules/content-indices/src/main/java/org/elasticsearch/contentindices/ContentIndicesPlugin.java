990d862b2d20a56a9c715a9a8b37be2b3678df70
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2022-04-14 22:51:54 +0200

    Register action and rest handler into the content index plugins.

--- modules/content-indices/src/main/java/org/elasticsearch/contentindices/ContentIndicesPlugin.java
+++ modules/content-indices/src/main/java/org/elasticsearch/contentindices/ContentIndicesPlugin.java
@@ -7,8 +7,51 @@
  */
 package org.elasticsearch.contentindices;
 
+import org.elasticsearch.action.ActionRequest;
+import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.IndexScopedSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.contentindices.action.GetContentIndicesAction;
+import org.elasticsearch.contentindices.action.GetContentIndicesTransportAction;
+import org.elasticsearch.contentindices.action.PutContentIndexAction;
+import org.elasticsearch.contentindices.action.PutContentIndexTransportAction;
+import org.elasticsearch.contentindices.rest.RestGetContentIndicesAction;
+import org.elasticsearch.contentindices.rest.RestPutContentIndexAction;
+import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestHandler;
+import java.util.List;
+import java.util.function.Supplier;
 
-public class ContentIndicesPlugin extends Plugin {
+public class ContentIndicesPlugin extends Plugin implements ActionPlugin {
+
+    @Override
+    public List<ActionPlugin.ActionHandler<? extends ActionRequest, ? extends ActionResponse>> getActions() {
+        return List.of(
+            new ActionPlugin.ActionHandler<>(PutContentIndexAction.INSTANCE, PutContentIndexTransportAction.class),
+            new ActionPlugin.ActionHandler<>(GetContentIndicesAction.INSTANCE, GetContentIndicesTransportAction.class)
+        );
+    }
+
+    @Override
+    public List<RestHandler> getRestHandlers(
+        Settings settings,
+        RestController restController,
+        ClusterSettings clusterSettings,
+        IndexScopedSettings indexScopedSettings,
+        SettingsFilter settingsFilter,
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        Supplier<DiscoveryNodes> nodesInCluster
+    ) {
+        return List.of(
+            new RestPutContentIndexAction(),
+            new RestGetContentIndicesAction()
+        );
+    }
 
 }
