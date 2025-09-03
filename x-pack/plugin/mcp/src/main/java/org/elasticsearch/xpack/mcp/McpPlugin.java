06290101186766e313ea7fc6b1d7d4e1d035901e
afoucret <aurelien.foucret@elastic.co>
2025-09-03 10:13:26 +0200

    Revisiting the decision to use the MCP SDK.

--- x-pack/plugin/mcp/src/main/java/org/elasticsearch/xpack/mcp/McpPlugin.java
+++ x-pack/plugin/mcp/src/main/java/org/elasticsearch/xpack/mcp/McpPlugin.java
@@ -7,45 +7,8 @@
 
 package org.elasticsearch.xpack.mcp;
 
-import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
-import org.elasticsearch.cluster.node.DiscoveryNodes;
-import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
-import org.elasticsearch.common.settings.ClusterSettings;
-import org.elasticsearch.common.settings.IndexScopedSettings;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsFilter;
-import org.elasticsearch.features.NodeFeature;
-import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.rest.RestController;
-import org.elasticsearch.rest.RestHandler;
-import org.elasticsearch.xpack.mcp.action.McpAction;
-import org.elasticsearch.xpack.mcp.action.TransportMcpAction;
-import org.elasticsearch.xpack.mcp.rest.RestMcpAction;
 
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+public class McpPlugin extends Plugin {
 
-public class McpPlugin extends Plugin implements ActionPlugin {
-
-    @Override
-    public List<ActionHandler> getActions() {
-        return List.of(new ActionHandler(McpAction.INSTANCE, TransportMcpAction.class));
-    }
-
-    @Override
-    public List<RestHandler> getRestHandlers(
-        Settings settings,
-        NamedWriteableRegistry namedWriteableRegistry,
-        RestController restController,
-        ClusterSettings clusterSettings,
-        IndexScopedSettings indexScopedSettings,
-        SettingsFilter settingsFilter,
-        IndexNameExpressionResolver indexNameExpressionResolver,
-        Supplier<DiscoveryNodes> nodesInCluster,
-        Predicate<NodeFeature> clusterSupportsFeature
-    ) {
-        return List.of(new RestMcpAction());
-    }
 }
